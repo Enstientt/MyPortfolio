9c5cda9bbe7ce46643c44f5dd49537c15af068d3
Zakariae Essadqui <zessadqu@e1r11p1.1337.ma>
2022-11-24 21:11:09 +0100

    assets

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./data";
 import { H1Welcome, MyImage, PWelcome, WelcomeContainer, WelcomeText } from "./Welcome.styled";
+import me from '../assets/me.jpeg'
 
 export const Welcome =()=>{
     const theme = useContext(ThemeContext);
     return (
         <WelcomeContainer>
-        <MyImage src="https://i.ibb.co/Hd1MjsV/Whats-App-Image-2022-11-15-at-4-28-29-PM.jpg" />
+        <MyImage src={me} />
         <WelcomeText >
             <H1Welcome theme={theme.theme}><b>👋I’m Zakariae Essadqui</b></H1Welcome>
             <PWelcome theme={theme.theme}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and typesetting</PWelcome>
--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,14 @@
 import React, { createContext } from "react";
+import safety from '../assets/safety-react.png'
+import fractol from '../assets/fractol.jpg'
+import push from '../assets/push_swap.jpg'
+import minitalk from '../assets/minitalk.webp'
+import philo from '../assets/philosophers.png'
+import intra from '../assets/intra.png'
+import calculator from '../assets/calReact.png'
+import cpge from '../assets/Cpge.webp'
+import insea from '../assets/Insea.jpg'
+import leet from '../assets/133.jpg'
 
 export const themes ={
 lightTheme : {
@@ -19,24 +29,24 @@ darkTheme:{
 
 export const ThemeContext = React.createContext(themes.darkTheme);
   export const slides = [
-    {url:"https://i.ibb.co/fNPYmY3/safety-react.png",name:"safety_app",name:"safety_app",description:"A baby step toward building a platform  to exchange currencies "
+    {url:safety,name:"safety_app",name:"safety_app",description:"A baby step toward building a platform  to exchange currencies "
 },
-{url:"https://i.ibb.co/1Jjz3zY/intra.png",name:"42Intra",description:"Since it s restricted for non-student of 42 network to access to the intranet  i m gonna give you a sneak-peek of how intranet looks like"
+{url:intra,name:"42Intra",description:"Since it s restricted for non-student of 42 network to access to the intranet  i m gonna give you a sneak-peek of how intranet looks like"
 },
-{url:"https://www.teahub.io/photos/full/9-91386_1920x1080-the-julia-set-data-id-250616-data.jpg",name:"Fractol",description:"The project was about creating a program on c that draw the famous mandelbrot set with infinit zomming feature "
+{url:fractol,name:"Fractol",description:"The project was about creating a program on c that draw the famous mandelbrot set with infinit zomming feature "
 },
-{url:"https://c4.wallpaperflare.com/wallpaper/586/447/591/fantasy-art-digital-art-science-fiction-artwork-wallpaper-preview.jpg",name:"Push_swap",description:"A pure algo project that i have the chance to test my logic on problem solving , it s about sorting a stack using a non-classic operations "
+{url:push,name:"Push_swap",description:"A pure algo project that i have the chance to test my logic on problem solving , it s about sorting a stack using a non-classic operations "
 },
-{url:"https://files.realpython.com/media/An-Overview-of-Concurrency-in-Python_Watermarked.c54c399ccb32.jpg",name:"Mini-talk",description:"The project main goal was to creat a server and client process for data transmission"
+{url:minitalk,name:"Mini-talk",description:"The project main goal was to creat a server and client process for data transmission"
 },
-{url:"https://www.interviewbit.com/blog/wp-content/uploads/2021/10/Banner-Dining-Philosophers-Problem.png",name:"Philosophers",description:"creating a programe on c that  solve the dining philosophers problem (multitasking and synchronistaion issue in Unix)"
+{url:philo,name:"Philosophers",description:"creating a programe on c that  solve the dining philosophers problem (multitasking and synchronistaion issue in Unix)"
 },
-{url:"https://i.ibb.co/7j7F9HW/calReact.png",name:"react_calculator",description:"calculator App built using ReactJs library , the first step toward learning the basics of react"
+{url:calculator,name:"react_calculator",description:"calculator App built using ReactJs library , the first step toward learning the basics of react"
 }
 ]
 
 export const parcour=[
-  {url:"https://i.f1g.fr/media/eidos/805x453_crop/2021/02/05/XVM701f7d7e-5b36-11eb-9ad1-c52cc1cea18c.jpg", parcoure:"2015-2017: I started my journey at the Higher School Preparatory Classes :two years of  intensive courses on math, physics and engineering science ",desc:"https://en.wikipedia.org/wiki/Classe_pr%C3%A9paratoire_aux_grandes_%C3%A9coles#:~:text=The%20classes%20pr%C3%A9paratoires%20aux%20grandes,French%20post%2Dsecondary%20education%20system.",name:"CPGE"},
-  {url:"https://i1.hespress.com/wp-content/uploads/2021/03/Institut-national-de-statistique-et-d-economie-appliquee.jpg", parcoure:"2017-2020 : I began my career as a statistical student enginner at The National Institute of Statistics and Applied Economics (INSEA)",desc:"https://en.wikipedia.org/wiki/National_Institute_of_Statistics_and_Applied_Economics",name:"INSEA"},
-  {url:"https://i.ytimg.com/vi/-qo0aC0koPw/maxresdefault.jpg", parcoure:"2021-until now: I started an exciting journey at 1337 IT school (42 network) for mor3 info about the school check the link below",desc:"https://1337.ma/en/",name:"1337"}
+  {url:cpge, parcoure:"2015-2017: I started my journey at the Higher School Preparatory Classes :two years of  intensive courses on math, physics and engineering science ",desc:"https://en.wikipedia.org/wiki/Classe_pr%C3%A9paratoire_aux_grandes_%C3%A9coles#:~:text=The%20classes%20pr%C3%A9paratoires%20aux%20grandes,French%20post%2Dsecondary%20education%20system.",name:"CPGE"},
+  {url:insea, parcoure:"2017-2020 : I began my career as a statistical student enginner at The National Institute of Statistics and Applied Economics (INSEA)",desc:"https://en.wikipedia.org/wiki/National_Institute_of_Statistics_and_Applied_Economics",name:"INSEA"},
+  {url:leet, parcoure:"2021-until now: I started an exciting journey at 1337 IT school (42 network) for mor3 info about the school check the link below",desc:"https://1337.ma/en/",name:"1337"}
 ]
